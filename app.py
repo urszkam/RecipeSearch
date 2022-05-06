4f9664d578be802182f3ae8bf0936a30ff337107
Ula <ursz.kam@gmail.com>
2022-05-06 17:14:02 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__, template_folder = 'templates')
+
+
+@app.route('/')
+def index():
+    # return 'Cześć, tu Python!'
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
